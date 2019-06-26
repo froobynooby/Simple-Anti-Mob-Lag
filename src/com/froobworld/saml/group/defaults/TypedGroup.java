e187bcac1c0f35ab1aa69b5e3a9b1c53e6b89e45
froobynooby <froobynooby@froobworld.com>
2019-06-26 13:03:37 +1000

    Add TypedGroup

---
+++ src/com/froobworld/saml/group/defaults/TypedGroup.java
@@ -0,0 +1,60 @@
+package com.froobworld.saml.group.defaults;
+
+import com.froobworld.saml.group.Group;
+import com.froobworld.saml.group.GroupStatusUpdater;
+import com.froobworld.saml.group.ProtoGroup;
+import com.froobworld.saml.group.TypedEntity;
+import org.bukkit.entity.EntityType;
+import org.bukkit.entity.LivingEntity;
+
+public class TypedGroup implements Group {
+    private EntityType type;
+    private double separationDistanceSquared;
+    private double minimumSize;
+
+    public TypedGroup(EntityType type, double separationDistance, double minimumSize) {
+        this.type = type;
+        this.separationDistanceSquared = Math.pow(separationDistance, 2);
+        this.minimumSize = minimumSize;
+    }
+    
+
+    @Override
+    public String getName() {
+        return "default_typed_" + type.name().toLowerCase();
+    }
+
+    @Override
+    public boolean inProtoGroup(LivingEntity entity, ProtoGroup protoGroup) {
+        return entity.getType() == type && entity.getLocation().distanceSquared(protoGroup.getCentre().getLocation()) <= separationDistanceSquared;
+    }
+
+    @Override
+    public boolean canBeCentre(LivingEntity entity) {
+        return entity.getType() == type;
+    }
+
+    @Override
+    public int assignTypeId(LivingEntity entity) {
+        return 0;
+    }
+
+    @Override
+    public GroupStatusUpdater groupStatusUpdater() {
+        return new GroupStatusUpdater() {
+            private int count;
+            private boolean group;
+
+            @Override
+            public void updateStatus(TypedEntity typedEntity) {
+                count++;
+                group = count >= minimumSize;
+            }
+
+            @Override
+            public boolean isGroup() {
+                return group;
+            }
+        };
+    }
+}
