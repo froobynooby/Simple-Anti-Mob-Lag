f5bc019d0e0400ea9a26ef8bd819cb8ddc1ab5e3
froobynooby <froobynooby@froobworld.com>
2019-07-27 10:21:58 +1000

    Remove member list from ProtoGroup GroupStatusUpdater makes keeping track of members redundant

--- src/com/froobworld/saml/group/ProtoGroup.java
+++ src/com/froobworld/saml/group/ProtoGroup.java
@@ -1,23 +1,17 @@
 package com.froobworld.saml.group;
 
-import java.util.*;
-
 public class ProtoGroup<T> {
     private GroupStatusUpdater<? super T> groupStatusUpdater;
     private T centre;
-    private List<T> members;
 
     public ProtoGroup(Group<? super T> group, T centre) {
         this.groupStatusUpdater = group.groupStatusUpdater();
         this.centre = centre;
-        this.members = new ArrayList<T>();
 
-        members.add(centre);
         groupStatusUpdater.updateStatus(centre);
     }
 
     public void addMember(T member) {
-        members.add(member);
         groupStatusUpdater.updateStatus(member);
     }
 
@@ -27,14 +21,6 @@ public class ProtoGroup<T> {
 
     public boolean isGroup() {
         return groupStatusUpdater.isGroup();
-    }
-
-    public int size() {
-        return members.size();
-    }
-
-    public Iterator<T> membersIterator() {
-        return members.iterator();
     }
 
 }
